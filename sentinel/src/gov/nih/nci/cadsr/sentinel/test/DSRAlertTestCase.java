504b3511b2ea137f8e2413efce576b49389e9eb7
hebell <hebell>
2006-02-01 16:27:17 +0000

    renamed package
    SVN-Revision: 168

---
+++ sentinel/src/gov/nih/nci/cadsr/sentinel/test/DSRAlertTestCase.java
@@ -0,0 +1,94 @@
+// Copyright (c) 2004 ScenPro, Inc
+
+// $Header: /share/content/gforge/sentinel/sentinel/src/gov/nih/nci/cadsr/sentinel/test/DSRAlertTestCase.java,v 1.4 2006-02-01 16:26:18 hebell Exp $
+// $Name: not supported by cvs2svn $
+
+package gov.nih.nci.cadsr.sentinel.test;
+
+import gov.nih.nci.cadsr.sentinel.tool.AlertBean;
+import gov.nih.nci.cadsr.sentinel.tool.Constants;
+import gov.nih.nci.cadsr.sentinel.tool.DBAlert;
+
+import java.util.PropertyResourceBundle;
+import java.util.ResourceBundle;
+
+import org.apache.struts.util.MessageResources;
+import org.apache.struts.util.MessageResourcesFactory;
+import org.apache.struts.util.PropertyMessageResources;
+
+
+import servletunit.struts.MockStrutsTestCase;
+
+/**
+ * This is the base test class which extends the Mock Struts Test Case class.
+ * 
+ * @author James McAndrew
+ */
+public class DSRAlertTestCase extends MockStrutsTestCase
+{
+  protected String _validUserid;
+  protected String _validPswd;
+  protected String _invalidUserid;
+  protected String _invalidPswd;
+
+  /**
+   * Constructor.
+   * 
+   * @param testName The name of the class for the test.
+   */
+  public DSRAlertTestCase(String testName)
+  {
+    super(testName);
+  }
+
+  /**
+   * The setUp method overrides the MockStrutsTestCase setUp to setup the
+   * datasource.
+   * 
+   * @throws Exception Any exceptions encountered during setup
+   */
+  public void setUp() throws Exception
+  {
+    super.setUp();
+
+    // Grab the useris/password combos from the properties file
+    ResourceBundle testProperties = PropertyResourceBundle.getBundle("gov.nih.nci.cadsr.sentinel.tool.test.DSRAlertTest");
+    _validUserid = testProperties.getString("valid.userid");
+    _validPswd = testProperties.getString("valid.pswd");
+    _invalidUserid = testProperties.getString("invalid.userid");
+    _invalidPswd = testProperties.getString("invalid.pswd");
+
+    // Setup the MessageResources object since we are only in a mock environment
+    getSession().getServletContext().setAttribute(
+        Constants._RESOURCES,
+        new PropertyMessageResources(MessageResourcesFactory.createFactory(),
+            "gov.nih.nci.cadsr.sentinel.DSRAlert"));
+  }
+
+  /**
+   * The setUpLoginSession method sets up the datasource that is normally
+   * created by the LogonForm and adds the alert bean to the session.
+   */
+  protected void setUpLoginSession()
+  {
+    // Get the default information needed to connect to the database.
+    // This requires an entry in the TNSNAMES.ORA file. If problems
+    // occur, first verify the database is accessible using the same
+    //information through SQL Plus.
+    MessageResources msgs = (MessageResources)getSession().getServletContext()
+        .getAttribute(Constants._RESOURCES);
+    String tnsname = msgs.getMessage(Constants._DBTNSNAME);
+    String username = msgs.getMessage(Constants._DBUSER);
+    String password = msgs.getMessage(Constants._DBPSWD);
+
+    // Setup the database pool.
+    DBAlert.setupPool(getSession(), tnsname, username, password);
+    getSession().setAttribute(AlertBean._SESSIONNAME,
+        new AlertBean(_validUserid, "", _validPswd));
+  }
+
+  protected String getPath(String page)
+  {
+    return ("/" + page);
+  }
+}
---
+++ sentinel/src/gov/nih/nci/cadsr/sentinel/test/TestLogon.java
@@ -0,0 +1,104 @@
+// Copyright (c) 2004 ScenPro, Inc
+
+// $Header: /share/content/gforge/sentinel/sentinel/src/gov/nih/nci/cadsr/sentinel/test/TestLogon.java,v 1.4 2006-02-01 16:26:18 hebell Exp $
+// $Name: not supported by cvs2svn $
+
+package gov.nih.nci.cadsr.sentinel.test;
+
+import gov.nih.nci.cadsr.sentinel.tool.AlertBean;
+import gov.nih.nci.cadsr.sentinel.tool.Constants;
+import gov.nih.nci.cadsr.sentinel.tool.LogonForm;
+
+/**
+ * Test the Alert Login logic.
+ * 
+ * @author James McAndrew
+ */
+public class TestLogon extends DSRAlertTestCase
+{
+    /**
+     * The main entry to run the test case.
+     * 
+     * @param args Command line arguments - none at this time.
+     */
+  public static void main(String[] args)
+  {
+    junit.textui.TestRunner.run(TestLogon.class);
+  }
+
+  /**
+   * Constructor
+   * 
+   * @param testName The name of the class to test.
+   */
+  public TestLogon(String testName)
+  {
+    super(testName);
+  }
+  
+  /**
+   * The setUp method overrides the DSRAlertTestCase setUp to setup the
+   * request path info and action form.
+   * 
+   * @throws Exception Any exceptions encountered during setup
+   */
+  public void setUp() throws Exception
+  {
+    super.setUp();
+
+    setRequestPathInfo(getPath(Constants._ACTLOGON));
+    setActionForm(new LogonForm());
+  }
+
+  /**
+   * The testSuccessfulLogin method is the test for a valid login.
+   */
+  public void testSuccessfulLogin()
+  {
+    // Setup a valid logon request
+    addRequestParameter("userid", _validUserid);
+    addRequestParameter("pswd", _validPswd);
+
+    // Then perform the action
+    actionPerform();
+
+    // Verify there were no errors and we got to the launcher page
+    verifyForward(Constants._ACTLIST);
+    verifyNoActionErrors();
+
+    // Verify there is a user bean in the session
+    assertNotNull(getSession().getAttribute(AlertBean._SESSIONNAME));
+  }
+
+  /**
+   * The testInvalidLogin method is the test for an invalid login.
+   */
+  public void testInvalidLogin()
+  {
+    // Setup an invalid logon request
+    addRequestParameter("userid", _invalidUserid);
+    addRequestParameter("pswd", _invalidPswd);
+
+    // Then perform the action
+    actionPerform();
+
+    // Verify there was an invalid password error and we got to the logon page
+    verifyActionErrors(new String[] { "DB.1017" });
+  }
+
+  /**
+   * The testBlankLogin method is the test for a blank login.
+   */
+  public void testBlankLogin()
+  {
+    // Setup an invalid logon request
+    addRequestParameter("userid", "");
+    addRequestParameter("pswd", "");
+
+    // Then perform the action
+    actionPerform();
+
+    // Verify there was an blank password error and we got to the logon page
+    verifyActionErrors(new String[] { "error.logon.blankuser" });
+  }
+}
