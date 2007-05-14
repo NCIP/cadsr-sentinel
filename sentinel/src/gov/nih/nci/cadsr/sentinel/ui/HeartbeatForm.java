35bde42e6f2e8c1b4b7106ae10658684317df4d2
hebell <hebell>
2007-05-14 22:21:45 +0000

    add heartbeat request for systems
    SVN-Revision: 283

---
+++ sentinel/src/gov/nih/nci/cadsr/sentinel/ui/HeartbeatForm.java
@@ -0,0 +1,44 @@
+// Copyright (c) 2007 ScenPro, Inc.
+
+// $Header: /share/content/gforge/sentinel/sentinel/src/gov/nih/nci/cadsr/sentinel/ui/HeartbeatForm.java,v 1.1 2007-05-14 22:21:45 hebell Exp $
+// $Name: not supported by cvs2svn $
+
+package gov.nih.nci.cadsr.sentinel.ui;
+
+import javax.servlet.http.HttpServletRequest;
+import org.apache.struts.action.ActionErrors;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionMapping;
+
+/**
+ * @author lhebel
+ *
+ */
+public class HeartbeatForm extends ActionForm
+{
+    private static final long serialVersionUID = 8909417350041490262L;
+
+    /**
+     * 
+     */
+    public HeartbeatForm()
+    {
+        super();
+    }
+
+    /**
+     * Validate the content of the Edit Screen.
+     * 
+     * @param mapping_
+     *        The action map defined for Edit.
+     * @param request_
+     *        The servlet request object.
+     * @return Any errors found.
+     */
+    public ActionErrors validate(ActionMapping mapping_, HttpServletRequest request_)
+    {
+        ActionErrors errors = new ActionErrors();
+        
+        return errors;
+    }
+}
