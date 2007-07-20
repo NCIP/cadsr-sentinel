4fe661f5018632a6911136037d830a228731d6c4
no_author <no_author@no_author.cvs2svn>
2007-07-20 19:52:19 +0000

    This commit was manufactured by cvs2svn to create tag 'sentinel_automation_pilot_1_0_0_0_build_28_'.
    SVN-Revision: 291

--- sentinel/scripts/autorun.cmd
+++
@@ -1,3 +0,0 @@
-echo $Header: /share/content/gforge/sentinel/sentinel/scripts/autorun.cmd,v 1.1 2007-07-19 15:26:52 hebell Exp $
-echo $Name: not supported by cvs2svn $
-%JAVA_HOME%\bin\java -client -classpath %BASE_DIR%\mail.jar;%BASE_DIR%\activation.jar;%BASE_DIR%\ojdbc14.jar;%BASE_DIR%\DSRAlert.jar gov.nih.nci.cadsr.sentinel.tool.AutoProcessAlerts
