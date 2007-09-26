43954f3303f56c7ee71c3a680187052489c91522
no_author <no_author@no_author.cvs2svn>
2007-09-26 13:42:25 +0000

    This commit was manufactured by cvs2svn to create tag 'v3_2_0_3_build_20_'.
    SVN-Revision: 485

--- sentinel/conf/template.update_options_url.sql
+++ sentinel/conf/template.update_options_url.sql
@@ -1,11 +1,11 @@
 /* Copyright ScenPro, Inc, 2005
 
-   $Header: /share/content/gforge/sentinel/sentinel/conf/template.update_options_url.sql,v 1.2 2007-07-26 19:27:03 hebell Exp $
+   $Header: /share/content/gforge/sentinel/sentinel/conf/template.update_options_url.sql,v 1.4 2007-09-26 13:42:20 hebell Exp $
    $Name: not supported by cvs2svn $
 
    Author: Larry Hebel
 
-   This script updates the Tool Options table with URL's for the Sandbox tier.
+   This script updates the Tool Options table with URL's for the @TIER.NAME@ tier.
 */
 set scan off;
 
@@ -31,12 +31,12 @@ insert into sbrext.tool_options_view_ext (tool_name, property, value, locale) VA
 insert into sbrext.tool_options_view_ext (tool_name, property, value, locale) VALUES ('CDEBrowser', 'VA_NDF_CODE', 'http://nciterms.nci.nih.gov:80/NCIBrowser/ConceptReport.jsp?dictionary=VA_NDFRT&code=', 'US');
 insert into sbrext.tool_options_view_ext (tool_name, property, value, locale) VALUES ('CURATION', 'REFDOC_FILEURL', 'http://cdecurate@TIER@.nci.nih.gov/filecache/', 'US');
 insert into sbrext.tool_options_view_ext (tool_name, property, value, locale) VALUES ('CURATION', 'URL', 'http://cdecurate@TIER@.nci.nih.gov', 'US');
-insert into sbrext.tool_options_view_ext (tool_name, property, value, locale) VALUES ('EVS', 'URL', 'http://cabio.nci.nih.gov/cacore32/http/remoteService', 'US');
+insert into sbrext.tool_options_view_ext (tool_name, property, value, locale) VALUES ('EVS', 'URL', 'http://cabio@TIER@.nci.nih.gov/cacore32/http/remoteService', 'US');
 insert into sbrext.tool_options_view_ext (tool_name, property, value, locale) VALUES ('FREESTYLE', 'URL', 'http://freestyle@TIER@.nci.nih.gov', 'US');
 insert into sbrext.tool_options_view_ext (tool_name, property, value, locale) VALUES ('SENTINEL', 'LINK.HTTP', 'http://cadsrsentinel@TIER@.nci.nih.gov/AlertReports/', 'US');
 insert into sbrext.tool_options_view_ext (tool_name, property, value, locale) VALUES ('SENTINEL', 'URL', 'http://cadsrsentinel@TIER@.nci.nih.gov', 'US');
 insert into sbrext.tool_options_view_ext (tool_name, property, value, locale) VALUES ('UMLBrowser', 'ADMIN_TOOL_URL', 'http://cadsradmin@TIER@.nci.nih.gov', 'US');
-insert into sbrext.tool_options_view_ext (tool_name, property, value, locale) VALUES ('UMLBrowser', 'CACORE_URL', 'http://cabio.nci.nih.gov/cacore32/http/remoteService', 'US');
+insert into sbrext.tool_options_view_ext (tool_name, property, value, locale) VALUES ('UMLBrowser', 'CACORE_URL', 'http://cabio@TIER@.nci.nih.gov/cacore32/http/remoteService', 'US');
 insert into sbrext.tool_options_view_ext (tool_name, property, value, locale) VALUES ('UMLBrowser', 'CDEBROWSER_TOOL_URL', 'http://cdebrowser@TIER@.nci.nih.gov', 'US');
 insert into sbrext.tool_options_view_ext (tool_name, property, value, locale) VALUES ('UMLBrowser', 'CDEBROWSER_URL', 'http://cdebrowser@TIER@.nci.nih.gov/CDEBrowser/', 'US');
 insert into sbrext.tool_options_view_ext (tool_name, property, value, locale) VALUES ('UMLBrowser', 'CURATION_TOOL_URL', 'http://cdecurate@TIER@.nci.nih.gov', 'US');
