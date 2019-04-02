8129790741505e76ed5ef5eff6e8c91611a544dd
asafieva <natalia.asafieva@nih.gov>
2019-04-02 18:02:27 -0400

    java11 branch initial changes

---
+++ docker11/README.md
@@ -0,0 +1,16 @@
+docker-compose configuration for cadsr-sentinel11
+=====
+
+## Requirements ##
+The following environment variables are required:
+BRANCH_OR_TAG=sentinel-4.0.7-sprint01-01
+tiername
+CADSR.DS.USER
+CADSR.DS.TNS.HOST
+CADSR.DS.TNS.PORT
+TOOL.BASE.DIR
+CADSR.DS.TNS.SID
+
+## To build ##
+1. cd docker directory
+2. docker-compose up -d
---
+++ docker11/run.sh
@@ -0,0 +1,45 @@
+#~/bin/sh
+echo "we are in the /usr/src/sentinel/cadsr-sentinel directory"
+git pull
+if [ $tag != 'origin/master'  ] && [ $tag != 'master' ]; then
+  git checkout tags/$tag
+fi
+
+# Function to check if wildfly is up #
+function wait_for_server() {
+  until `/opt/wildfly/bin/jboss-cli.sh -c --controller=localhost:19990 ":read-attribute(name=server-state)" 2> /dev/null | grep -q running`; do
+    sleep 1
+  done
+}
+
+cd software
+
+echo "=> build application and copy artifacts to /local/content/sentinel"
+ant -file build.xml build-product
+
+cp deployment-artifacts/cadsrsentinel.war /local/content/cadsrsentinel/bin
+
+echo "=> starting wildfly in background"
+/opt/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0 &
+
+echo "=> Waiting for the server to boot"
+wait_for_server
+
+echo "=> deploying modules"
+/opt/wildfly/bin/jboss-cli.sh --file=cadsrsentinel_modules11.cli
+
+echo "=> reloading wildfly"
+/opt/wildfly/bin/jboss-cli.sh --connect controller=localhost:19990 command=:reload
+
+echo "=> Waiting for the server to reload"
+wait_for_server
+
+echo "=> deploying"
+/opt/wildfly/bin/jboss-cli.sh --file=cadsrsentinel_modules11.cli
+/opt/wildfly/bin/jboss-cli.sh --file=cadsrsentinel_setup_deploy.cli
+
+echo "=> shutting wildfly down"
+/opt/wildfly/bin/jboss-cli.sh --connect controller=localhost:19990 command=:shutdown
+
+echo "=> starting wildfly in foreground"
+/opt/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0 
